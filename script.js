f2f3cdbc321877f377d9421248a9fc3cc0986282
Krithick Raj S <krithicks2007@gmail.com>
2025-11-13 22:07:20 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let index = 0;
+showSlides();
+
+function showSlides() {
+  const slides = document.getElementsByClassName("slide");
+  
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+
+  index++;
+  if (index > slides.length) { index = 1 }
+
+  slides[index - 1].style.display = "block";
+
+
+  setTimeout(showSlides, 4000);
+}
